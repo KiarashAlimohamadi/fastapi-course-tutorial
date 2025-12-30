d167e0dad2fbe669cc2e9e9ddeb82747a1e05954
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2025-12-30 12:02:25 +0330

    create user table for database

--- core/database_test.py
+++ core/database_test.py
@@ -1,5 +1,13 @@
-from sqlalchemy import create_engine
+#--------------IMPORTS---------------------------------------------------
+
+from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.orm import sessionmaker,declarative_base
+
+#-------------------------------------------------------------------------
+
+
+#--------------ُSTARTING DATABASE---------------------------------------------------
+
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
@@ -17,6 +25,18 @@ create base class for declaring tables
 """
 Base = declarative_base()
 
+#---------------------------------------------------------------------------------
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    age = Column(Integer)
+
+    def __repr__(self):
+        return f"User{self.id},first name{self.first_name},last name{self.last_name}"
 
 
 """
